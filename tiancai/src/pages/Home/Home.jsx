b72785ad65ade2f96118b2a44db63471308b0968
zcxzcx <729796011@qq.com>
2019-01-31 16:26:34 +0800

    home完成

--- tiancai/src/pages/Home/Home.jsx
+++ tiancai/src/pages/Home/Home.jsx
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 //css
 import './Home.css';
 import '../../assets/index.css';
+import '../../assets/swiper.css';
+//引入子组件
+import Hheader from '../../containers/Hheader/Hheader.jsx';
+import SignIn from '../../containers/SignIn/SignIn.jsx';
+import Swiper from '../../containers/Swiper/Swiper.jsx';
+import Cenbox from '../../containers/Cenbox/Cenbox.jsx';
+import Introduction from '../../containers/Introduction/Introduction.jsx';
+// import Footer from '../../containers/Footer/Footer.jsx';
+
 class Home extends Component {
   //数据
   constructor(props) {
@@ -17,9 +26,14 @@ class Home extends Component {
   //html
   render() {
     return (
-
-
-
+      <div>
+        <SignIn />
+        <Hheader />
+        <Swiper />
+        <Cenbox />
+        <Introduction />
+        {/* <Footer /> */}
+      </div>
     );
   }
 }
